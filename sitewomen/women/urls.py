4772fe804c84317e21887aded1182455ec542a7f
ihorko95 <ihorko95@gmail.com>
2023-12-25 18:32:05 +0200

    first commit

---
+++ sitewomen/women/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from women import views
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
+    path('cats/<int:cat_id>/', views.categories),
+
+]
